ded344d12167d12eb5c82bbe092e8201e1d06125
Chris Anderson <jchris@grabb.it>
2009-10-13 19:40:01 -0700

    view of recent reports

---
+++ views/recent/map.js
@@ -0,0 +1,17 @@
+function(doc) {
+  if (doc.tests && doc.platform) {
+    // count pass fail
+    var pass = 0; fail = 0;
+    for (var i=0; i < doc.tests.length; i++) {
+      if (doc.tests[i].status == "success") {
+        pass++;
+      } else {
+        fail++;
+      }
+    };
+    emit([doc.node.version, doc.timestamp], {
+      pass : pass,
+      fail : fail
+    })
+  }
+};
